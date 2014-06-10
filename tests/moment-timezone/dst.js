f2162320d04ef72d9c0c05a2a5c51c0f55f63a7c
Steve Hofmeyr <stevehof@users.noreply.github.com>
2014-06-11 01:37:35 +0200

    Fix tests to use utc

--- tests/moment-timezone/dst.js
+++ tests/moment-timezone/dst.js
@@ -13,9 +13,9 @@ exports.dst = {
     t.equal(moment.tz("2014-03-09T09:45:00", "America/New_York").format(), "2014-03-09T09:45:00-04:00", "2014-03-09T09:45:00 in America/New_York should be 2014-03-09T09:45:00-04:00");
 
 		//From UTC
-		t.equal(moment("2014-03-09T07:45:00").tz("America/New_York").format(), "2014-03-09T00:45:00-05:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
-    t.equal(moment("2014-03-09T08:45:00").tz("America/New_York").format(), "2014-03-09T01:45:00-05:00", "2014-03-09T03:45:00 in America/New_York should be 2014-03-09T03:45:00-04:00");
-    t.equal(moment("2014-03-09T09:45:00").tz("America/New_York").format(), "2014-03-09T03:45:00-04:00", "2014-03-09T04:45:00 in America/New_York should be 2014-03-09T04:45:00-04:00");
+		t.equal(moment.utc("2014-03-09T05:45:00").tz("America/New_York").format(), "2014-03-09T00:45:00-05:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
+    t.equal(moment.utc("2014-03-09T06:45:00").tz("America/New_York").format(), "2014-03-09T01:45:00-05:00", "2014-03-09T03:45:00 in America/New_York should be 2014-03-09T03:45:00-04:00");
+    t.equal(moment.utc("2014-03-09T07:45:00").tz("America/New_York").format(), "2014-03-09T03:45:00-04:00", "2014-03-09T04:45:00 in America/New_York should be 2014-03-09T04:45:00-04:00");
 
 
 		// AMBIGUOUS HOUR
@@ -27,11 +27,12 @@ exports.dst = {
 
 
 		//From UTC
-		t.equal(moment("2014-11-02T06:45:00").tz("America/New_York").format(), "2014-11-02T00:45:00-04:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
-		t.equal(moment("2014-11-02T07:45:00").tz("America/New_York").format(), "2014-11-02T01:45:00-04:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
-		t.equal(moment("2014-11-02T08:45:00").tz("America/New_York").format(), "2014-11-02T01:45:00-05:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
-    t.equal(moment("2014-11-02T09:45:00").tz("America/New_York").format(), "2014-11-02T02:45:00-05:00", "2014-03-09T03:45:00 in America/New_York should be 2014-03-09T03:45:00-04:00");
-    t.equal(moment("2014-11-02T10:45:00").tz("America/New_York").format(), "2014-11-02T03:45:00-05:00", "2014-03-09T04:45:00 in America/New_York should be 2014-03-09T04:45:00-04:00");
+		t.equal(moment.utc("2014-11-02T04:45:00").tz("America/New_York").format(), "2014-11-02T00:45:00-04:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
+		t.equal(moment.utc("2014-11-02T05:45:00").tz("America/New_York").format(), "2014-11-02T01:45:00-04:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
+		t.equal(moment.utc("2014-11-02T06:45:00").tz("America/New_York").format(), "2014-11-02T01:45:00-05:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
+		t.equal(moment.utc("2014-11-02T07:45:00").tz("America/New_York").format(), "2014-11-02T02:45:00-05:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
+		t.equal(moment.utc("2014-11-02T08:45:00").tz("America/New_York").format(), "2014-11-02T03:45:00-05:00", "2014-03-09T02:45:00 in America/New_York should be 2014-03-09T02:45:00-04:00");
+
 
 
     t.done();
